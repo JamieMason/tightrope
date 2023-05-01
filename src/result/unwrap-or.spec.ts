70f9402614a3458c9a43614fa60e0ea538b9ded4
Jamie Mason <jamie@foldleft.io>
2023-05-01 10:41:45 +0100

    fix(result): unwrapOr accepts default value

--- src/result/unwrap-or.spec.ts
+++ src/result/unwrap-or.spec.ts
@@ -3,14 +3,10 @@ import { Err, Ok } from '.';
 import { pipe } from '../fn/pipe';
 import { unwrapOr } from './unwrap-or';
 
-it('returns the provided Ok when Err', () => {
-  expect(pipe(Err.create(new Error('wat?')), unwrapOr(Ok.create(2)))).toEqual(
-    2,
-  );
+it('returns the provided fallback when Err', () => {
+  expect(pipe(Err.create(new Error('wat?')), unwrapOr(2))).toEqual(2);
 });
 
 it('keeps the original Ok when Ok', () => {
-  expect(
-    pipe(Ok.create(2), unwrapOr(Ok.create<number, Error>(4)), (x) => x),
-  ).toEqual(2);
+  expect(pipe(Ok.create(2), unwrapOr(4), (x) => x)).toEqual(2);
 });
