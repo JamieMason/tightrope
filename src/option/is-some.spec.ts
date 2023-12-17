2808a0662d8dd2629d895f9027dd183ca85deb34
Jamie Mason <jamie@foldleft.io>
2023-12-17 16:31:36 +0000

    strong type option isSome/None

--- src/option/is-some.spec.ts
+++ src/option/is-some.spec.ts
@@ -1,14 +1,23 @@
 import { expect, it } from 'vitest';
-import { none, Some } from '.';
+import { Some, none } from '.';
 import { isSome } from './is-some';
 
 it('isSome should return true when Some is provided', () => {
-  const someValue = new Some(5);
-  const result = isSome(someValue);
-  expect(result).toBe(true);
+  expect.assertions(1);
+  const someValue: unknown = new Some(5);
+  if (isSome(someValue)) {
+    expect<Some<unknown>>(someValue).toEqual(new Some(5));
+  }
 });
 
 it('isSome should return false when None is provided', () => {
-  const result = isSome(none);
-  expect(result).toBe(false);
+  expect(isSome(none)).toEqual(false);
+});
+
+it('isSome should return false and preserve type when number is provided', () => {
+  expect.assertions(1);
+  const someValue: number = 8;
+  if (isSome(someValue) === false) {
+    expect<number>(someValue).toEqual(8);
+  }
 });
--- src/option/is-some.ts
+++ src/option/is-some.ts
@@ -3,20 +3,8 @@ import { Some } from '.';
 /**
  * Determines whether a value is an instance of `Some`.
  *
- * ## Example
- *
- * ```ts
- * import { isSome } from 'tightrope/option/is-some';
- * import { Some } from 'tightrope/option';
- *
- * console.log(isSome(new Some('hello'))); // true
- * console.log(isSome('not a some')); // false
- * ```
- *
- * @param value The value to check.
- * @returns `true` if the value is a `Some`, `false` otherwise.
  * @tags option, guard
  */
-export function isSome<Value>(value: unknown): value is Some<Value> {
+export function isSome<T>(value: unknown): value is Some<T> {
   return value instanceof Some && value._tag === 'Some';
 }
