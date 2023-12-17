70078a462d5b4e19caaaaed702f4d7ef04ff398d
Jamie Mason <jamie@foldleft.io>
2023-12-17 21:13:07 +0000

    isJestEqual

--- src/guard/is-jest-equal.ts
+++ src/guard/is-jest-equal.ts
@@ -2,10 +2,26 @@ import { curry } from '../fn/curry';
 import type { AsymmetricMatcher } from '../fn/types';
 import { isAsymmetricMatcher } from './is-asymmetric-matcher';
 
+export type IsJestEqual = {
+  <T>(other: T): {
+    (
+      value: unknown,
+    ): value is typeof other extends AsymmetricMatcher<infer T>
+      ? T
+      : typeof other;
+  };
+  <T>(
+    other: T,
+    value: unknown,
+  ): value is typeof other extends AsymmetricMatcher<infer T>
+    ? T
+    : typeof other;
+};
+
 /** ... */
 export const isJestEqual = curry(
-  (
-    other: unknown,
+  <T>(
+    other: T,
     value: unknown,
   ): value is typeof other extends AsymmetricMatcher<infer T>
     ? T
@@ -13,4 +29,4 @@ export const isJestEqual = curry(
     other === value ||
     (isAsymmetricMatcher(other) && other.asymmetricMatch(value)),
   2,
-);
+) as IsJestEqual;
