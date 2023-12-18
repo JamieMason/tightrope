c2b9e9fff47e1f6ee129c86757f3f07ffb9e39d3
Jamie Mason <jamie@foldleft.io>
2023-12-18 20:01:09 +0000

    fix test

--- src/guard/is-array.spec.ts
+++ src/guard/is-array.spec.ts
@@ -5,7 +5,7 @@ it('asserts value is an array, preserving its type', () => {
   expect.assertions(1);
   const knownArray = [1, 2];
   if (isArray(knownArray)) {
-    expect<number[]>(knownArray).toEqual(true);
+    expect<number[]>(knownArray).toEqual(knownArray);
   }
 });
 
@@ -13,7 +13,7 @@ it('asserts unknown type is an array', () => {
   expect.assertions(1);
   const unknownType: unknown = [1, 2];
   if (isArray(unknownType)) {
-    expect<unknown[]>(unknownType).toEqual(true);
+    expect<unknown[]>(unknownType).toEqual(unknownType);
   }
 });
 
@@ -21,7 +21,7 @@ it('asserts unknown array is an array', () => {
   expect.assertions(1);
   const unknownArray: unknown[] = [1, 2];
   if (isArray(unknownArray)) {
-    expect<unknown[]>(unknownArray).toEqual(true);
+    expect<unknown[]>(unknownArray).toEqual(unknownArray);
   }
 });
 
