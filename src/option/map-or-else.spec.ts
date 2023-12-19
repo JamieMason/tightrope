8f2e8cb3bc227b8b6c508549a48ea324d17893a1
Jamie Mason <jamie@foldleft.io>
2023-12-19 02:17:09 +0000

    [WIP]

--- src/option/map-or-else.spec.ts
+++ src/option/map-or-else.spec.ts
@@ -1,9 +1,8 @@
 import { expect, it } from 'vitest';
-import { none, Some } from '.';
-import { mapOrElse } from './map-or-else';
+import { mapOrElse, just, nothing } from '../rescript/maybe.gen';
 
 it('mapOrElse applies mapFn when input is Some', () => {
-  const opt = new Some(2);
+  const opt = just(2);
   const mapFn = (x: number) => x * 2;
   const defaultFn = () => -1;
   expect(mapOrElse(mapFn, defaultFn, opt)).toEqual(4);
@@ -12,11 +11,11 @@ it('mapOrElse applies mapFn when input is Some', () => {
 it('mapOrElse applies defaultFn when input is None', () => {
   const mapFn = (x: number) => x * 2;
   const defaultFn = () => -1;
-  expect(mapOrElse(mapFn, defaultFn, none)).toEqual(-1);
+  expect(mapOrElse(mapFn, defaultFn, nothing)).toEqual(-1);
 });
 
 it('mapOrElse handles complex types', () => {
-  const opt = new Some({ value: 2 });
+  const opt = just({ value: 2 });
   const mapFn = (obj: { value: number }) => obj.value * 2;
   const defaultFn = () => -1;
   expect(mapOrElse(mapFn, defaultFn, opt)).toEqual(4);
--- src/rescript/Maybe.res
+++ src/rescript/Maybe.res
@@ -3,6 +3,11 @@ type maybe<'a> =
   | Just('a)
   | Nothing(int)
 
+// type branch = {
+//   onJust: 'a => 'b,
+//   onNothing: unit => 'b,
+// }
+
 @genType
 let mapOrElse = (onJust, onNothing, val) =>
   switch val {
@@ -16,3 +21,9 @@ let andThen = (onJust, onNothing, val) =>
   | Just(value) => onJust(value)
   | Nothing(_) => onNothing()
   }
+
+@genType
+let just = value => Just(value)
+
+@genType
+let nothing = Nothing(0)
