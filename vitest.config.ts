3913bd0c33eb664176698c0aed79bafe9b3f48c1
Jamie Mason <jamie@foldleft.io>
2024-08-03 12:50:27 +0100

    chore(tsc): update compiler options

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,29 +1,29 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ['src/**/*.spec.ts'],
+    include: ["src/**/*.spec.ts"],
     coverage: {
       all: true,
       enabled: true,
-      include: ['src/**/*.ts'],
+      include: ["src/**/*.ts"],
       exclude: [
         // these files only contain types and although used in tests, show 0
         // coverage
-        'src/**/index.ts',
-        'src/**/types.ts',
+        "src/**/index.ts",
+        "src/**/types.ts",
         // this is not source code
-        '**/*.spec.ts',
+        "**/*.spec.ts",
       ],
-      extension: ['.ts'],
-      provider: 'v8',
-      reporter: ['lcov', 'html', 'text'],
+      extension: [".ts"],
+      provider: "v8",
+      reporter: ["lcov", "html", "text"],
       thresholds: {
         autoUpdate: true,
-        branches: 97.1,
+        branches: 97.09,
         functions: 87.64,
-        lines: 97.65,
-        statements: 97.65,
+        lines: 97.63,
+        statements: 97.63,
       },
     },
   },
